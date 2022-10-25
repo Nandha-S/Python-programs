d16f6c5d296e83cb473a6c1f1787fbf4a8278659
nandhakumar <nandhakumar.somu@gmail.com>
2022-10-25 17:42:22 +0530

    java prgrams

---
+++ Reverse.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Reverse{
+public static void main(String args[]){
+Scanner input=new Scanner(System.in);
+System.out.println("enter the number:");
+int number=input.nextInt();
+int rem,sum=0;
+while(number>0){
+	rem=number%10;
+	sum=(sum*10)+rem;
+	number=number/10;
+}
+System.out.println(sum);
+}
+}
